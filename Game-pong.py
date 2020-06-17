432958b1a21ea54e0d313a3363bea0f4f3419011
Amin <aminfatehi1399@gmail.com>
2020-06-17 16:57:20 +0430

    add border checking

--- Game-pong.py
+++ Game-pong.py
@@ -34,9 +34,10 @@ ball.speed(0)
 ball.shape("square")
 ball.color("green")
 ball.penup()
-ball.goto(0 ,0 )
-ball.dx = 2
-ball.dy = 2
+ball.goto(0 , 0)
+ball.dx = 0.2
+ball.dy = 0.2
+
 # function
 def paddle_a_up():
     y = paddle_a.ycor()
@@ -74,3 +75,23 @@ while True:
     # move the ball
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
+
+
+    # border checkking
+
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+
+    if ball.xcor() > 390:
+        ball.goto(0 , 0)
+        ball.dx *= -1
+
+    if ball.xcor() < -390:
+        ball.goto(0 , 0)
+        ball.dx *= -1
