334ebfacd2428a24da5a98ffafc68c6c338d5fe3
Amin <aminfatehi1399@gmail.com>
2020-06-17 16:07:03 +0430

    Add paddle A and paddle B

--- Game-pong.py
+++ Game-pong.py
@@ -10,6 +10,24 @@ wn.bgcolor("black")
 wn.setup(width = 800 , height = 600)
 wn.tracer(0)
 
+# paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("red")
+paddle_a.shapesize(stretch_wid=3 , stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350 , 0)
+# paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("blue")
+paddle_b.shapesize(stretch_wid=3 , stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350 ,0 )
+# ball
+
 # Main game loop
 while True:
     wn.update()
