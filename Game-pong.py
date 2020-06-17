d7ff32c4e80538d430617e54b4d58819a05d0136
Amin <aminfatehi1399@gmail.com>
2020-06-17 16:20:14 +0430

    add function and keyboard binding paddle_a

--- Game-pong.py
+++ Game-pong.py
@@ -18,6 +18,7 @@ paddle_a.color("red")
 paddle_a.shapesize(stretch_wid=3 , stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350 , 0)
+
 # paddle B
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
@@ -26,6 +27,7 @@ paddle_b.color("blue")
 paddle_b.shapesize(stretch_wid=3 , stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350 ,0 )
+
 # ball
 ball = turtle.Turtle()
 ball.speed(0)
@@ -33,6 +35,23 @@ ball.shape("square")
 ball.color("green")
 ball.penup()
 ball.goto(0 ,0 )
+
+# function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+# keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "1")
+wn.onkeypress(paddle_a_down, "2")
+
 # Main game loop
 while True:
     wn.update()
